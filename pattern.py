43b25a13c8d8ebf39e96518170c748a51969888b
SinghBanta <singhbanta84@gmail.com>
2025-01-28 20:03:03 +0530

    add pattern question

---
+++ pattern.py
@@ -0,0 +1,82 @@
+#binary tree at coding ninja
+# 1111
+# 000
+# 11
+# 0
+
+# def printPatt(n):
+#     for i in range(n):
+#         for j in range(n-i):
+#             if(i%2==0):
+#                 print('1',end="")
+#             else:
+#                 print('0',end="")
+#         print("\n")
+        
+
+# n=4
+# print(printPatt(n))
+
+
+#draw a pattern 
+# 1111
+# 222
+# 33
+# 4
+
+# n=int(input())
+# l=1
+# for i in range(n):
+#     for j in range(n-i):
+#         print(l,end="")
+#     print("\n")
+#     l += 1
+
+
+#draw pattern
+# 1010
+# 101
+# 10
+# 1
+
+# n=int(input("Enter a number: "))
+
+# for i in range(n):
+#     for j in range(n-i):
+#         if(j%2==0):
+#             print("1",end="")
+#         else:
+#             print("0",end="")
+#     print("\n")      
+
+
+#draw a pattern 
+# 1234
+# 567
+# 89
+#10
+
+# n=int(input("Enter a number"))
+# l=1
+# for i in range(n):
+#     for j in range(n-i):
+#         print(l,end='')
+#         l=l+1
+#     print("\n") 
+    
+    
+#draw a pattern
+# 1234
+# 123
+# 12
+# 1
+
+# n=int(input("Enter a number"))
+# for i in range(n):
+#     for j in range(1, n-i+1):
+#         print(j, end='')
+#     print("\n")
+
+
+
+
--- str.py
+++ str.py
@@ -120,20 +120,16 @@
 
 
 
-#binary tree at coding ninja
 
-# def printPatt(n):
-#     for i in range(n):
-#         for j in range(n-i):
-#             if(i%2==0):
-#                 print('1',end="")
-#             else:
-#                 print('0',end="")
-#         print("\n")
+
+
+
+      
+        
+        
+        
         
 
-# n=4
-# print(printPatt(n))
 
 
 
